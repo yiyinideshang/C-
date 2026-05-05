100e01ca3ee876f172e980a9559f5441a533f3cd
yiyinideshang <2779271357@qq.com>
2026-05-05 17:47:36 +0800

    2026-5-5-1：更新了\刷题\力扣C++ 目录下的相关内容 - 新增了\LeetCode 目录下的 “二叉搜索树中第K小的元素” 代码 - 更新了\力扣二叉树.md 文档 - 更新了\力扣二叉树2.md 文档

---
+++ 刷题/力扣C++/LeetCode/二叉搜索树中第K小的元素.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+
+// 二叉树节点的定义:
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+// 根据层序遍历序列构建二叉树（"null" 表示空节点）
+TreeNode* buildPreorder(const vector<string>& nodes,int& index){
+    if (nodes.empty() || nodes[0] == "null") return nullptr;
+
+    // 创建根节点
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+
+    int i = 1;  // 从第二个元素开始处理
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* current = q.front();
+        q.pop();
+
+        // 处理左子节点
+        if (i < nodes.size() && nodes[i] != "null") {
+            current->left = new TreeNode(stoi(nodes[i]));
+            q.push(current->left);
+        }
+        i++;
+
+        // 处理右子节点
+        if (i < nodes.size() && nodes[i] != "null") {
+            current->right = new TreeNode(stoi(nodes[i]));
+            q.push(current->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+// 便利包装：隐藏 index 引用
+TreeNode* buildTree(const vector<string>& preorder){
+    int index = 0;
+    return buildPreorder(preorder,index);
+}
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> stk;
+        while(root != nullptr || !stk.empty()){
+            // 一路向左，将所有左节点入栈
+            while(root!=nullptr){
+                stk.push(root);
+                root = root->left;
+            }
+            // 弹出栈顶节点，此时即为中序访问的节点
+            root = stk.top();
+            stk.pop();
+            //取出栈顶节点后,k的值-1
+            --k;
+            if(k == 0){//如果k的值为0,则表示当前节点的元素值就是原第K小的元素
+                break;
+            }
+            //转向右子树
+            root = root->right;
+        }
+        return root->val;//函数返回为该节点的元素值
+    }
+};
+
+//后序释放
+void deleteTree(TreeNode* root){
+    if(!root)
+        return;//空节点本身就指向nullptr,不用释放
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
+}
+
+int main()
+{
+    //通过vector构建二叉树
+    //预先将层序遍历序列存入 vector（"null" 表示空节点）
+    vector<string> preorder = {"3","1","4","null","2"};
+    TreeNode* root = buildTree(preorder);
+
+    int k = 2;
+    int result = 0;
+    Solution s;
+    result = s.kthSmallest(root,k);
+    std::cout<<result<<std::endl;
+
+    deleteTree(root);
+    return 0;   
+}
